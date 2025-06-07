f6c89fc5d1081b082a05f4542d1a67ab19c83823
Blaise Baptist <blaise.baptist@gmail.com>
2025-06-07 12:59:14 -0400

    made sd

--- main.py
+++ main.py
@@ -3,51 +3,36 @@ import json
 
 import pandas as pd
 
+debug = True
 def get_item_orders(item_url):
     request_url = url + "/" + item_url + "/orders"
     response = requests.get(request_url)
     orders =  pd.json_normalize(response.json()['payload']['orders'])[['platinum','quantity','order_type','user.status']]
     online_mask = (orders['user.status'] != 'offline')
+
     buy_orders_mask = (orders['order_type'] == 'buy')
-    sell_orders_mask = (orders['order_type'] == 'sell')
-    sell_orders = orders.loc[online_mask].loc[sell_orders_mask].sort_values(by='platinum')[['platinum','quantity']]
     buy_orders = orders.loc[online_mask].loc[buy_orders_mask].sort_values(by='platinum')[['platinum','quantity']]
-
-
     buy_count = buy_orders['quantity'].sum()
     buy_mean = buy_orders.apply(lambda x: x.platinum*x.quantity, axis=1).sum()/buy_count
+    buy_sd = pow(buy_orders.apply(lambda x: pow(x.platinum*x.quantity - buy_mean,2),axis = 1).sum()/buy_count,0.5)
+
+    sell_orders_mask = (orders['order_type'] == 'sell')
+    sell_orders = orders.loc[online_mask].loc[sell_orders_mask].sort_values(by='platinum')[['platinum','quantity']]
     sell_count = sell_orders['quantity'].sum()
     sell_mean = sell_orders.apply(lambda x: x.platinum*x.quantity, axis=1).sum()/sell_count
-    print("sell orders\n",sell_orders)
-    print("sell mean",sell_mean)
-    print("sell count",sell_count)
+    sell_sd = pow(sell_orders.apply(lambda x: pow(x.platinum*x.quantity - sell_mean,2),axis = 1).sum()/sell_count,0.5)
+    if debug:
+        print("buy orders\n",buy_orders)
+        print("buy mean",  buy_mean)
+        print("buy count",buy_count)
+        print("buy sd",buy_sd)
 
-    print("buy orders\n",buy_orders)
-    print("buy mean",  buy_mean)
-    print("buy count",buy_count)
-
-    return
+        print("sell orders\n",sell_orders)
+        print("sell mean",sell_mean)
+        print("sell count",sell_count)
+        print("sell sd",sell_sd)
 
     # Remove Outliers
-
-    buy_outlier_condition = lambda x: x['platinum'] < buy_mean
-    sell_outlier_condition = lambda x: x['platinum'] > sell_mean
-
-    buy_orders = [x for x in buy_orders if not buy_outlier_condition(x)]
-    sell_orders = [x for x in sell_orders if not sell_outlier_condition(x)]
-
-    buy_volume = len(buy_orders)
-    sell_volume = len(sell_orders)
-
-    print("SELL ORDERS:")
-    for sell_order in sell_orders:
-        print(sell_order)
-
-    print(buy_mean)
-    print(sell_mean)
-    print(buy_volume)
-    print(sell_volume)
-    print(item_url)
 
 url = "https://api.warframe.market/v1/items"
 response = requests.get(url)
